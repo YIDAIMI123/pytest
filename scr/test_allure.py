d6ffe44e71cc07ea453eeae0374d280c1daba892
ASL <15945232+ASL114514@user.noreply.gitee.com>
2025-12-10 17:55:47 +0800

    update

--- scr/test_allure.py
+++ scr/test_allure.py
@@ -4,7 +4,7 @@ import pytest
 @pytest.mark.parametrize("a,b,sum",[    #这是定义在测试函数上的，@fixture是定义在普通函数上(不是定义在测试函数上)
     (1,2,3),
     (4,5,9),
-    (100,6,16),
+    (100,6,160),
 ])
 def test_add(a,b,sum):
     assert a + b == sum
